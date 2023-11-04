602c6a5b4672a46dc32a727ef032bfeb95908a00
Bartosz Nowak <bartoszn337@gmail.com>
2023-11-04 18:55:21 +0100

    package.json reborn

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -3,8 +3,8 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import {configDotenv} from "dotenv"
 configDotenv();
 
-const ZKEVM_TESTNET_RPC_URL = process.env.ZKEVM_TESTNET_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "";
+const ZKEVM_TESTNET_RPC_URL = process.env.ZKEVM_TESTNET_RPC_URL ?? "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "0000000000000000000000000000000000000000000000000000000000000000"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
