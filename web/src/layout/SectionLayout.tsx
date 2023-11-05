647b4099c35110604596df5dd524147602c03d56
Kodziak <kodziak1416@gmail.com>
2023-11-05 06:45:34 +0000

    xd

--- web/src/layout/SectionLayout.tsx
+++ web/src/layout/SectionLayout.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export function SectionLayout({ children }: Props) {
   return (
-    <main className="flex min-h-screen h-[100vh] m-32 gap-64">
+    <main className="flex min-h-screen items-center mx-32 gap-64">
       <SideNav />
 
       <div className="w-[600px] flex flex-col gap-8">{children}</div>
