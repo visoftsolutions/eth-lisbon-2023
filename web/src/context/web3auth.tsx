822aa5cecf9c99302af376618b384a1e96c7f089
Bartosz Nowak <bartoszn337@gmail.com>
2023-11-05 02:44:34 +0100

    exports

--- web/src/context/web3auth.tsx
+++ web/src/context/web3auth.tsx
@@ -2,13 +2,13 @@ import { AuthKitSignInData, Web3AuthModalPack } from "@safe-global/auth-kit";
 import { UserInfo } from "@web3auth/base";
 import React, { createContext, useState, useContext, ReactNode } from "react";
 
-interface Web3Auth {
+export interface Web3Auth {
   web3AuthModalPack?: Web3AuthModalPack;
   authKitSignInData?: AuthKitSignInData;
   userInfo?: Partial<UserInfo>;
 }
 
-const Web3AuthContext = createContext<{
+export const Web3AuthContext = createContext<{
   web3Auth: Web3Auth;
   setWeb3Auth: (value: Web3Auth) => void;
 }>({ web3Auth: {}, setWeb3Auth: () => {} });
